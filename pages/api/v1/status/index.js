bc8d39eaa80d2345719be670746b4f46a96781fe
Pedro <138155992+pedroaugsf@users.noreply.github.com>
2025-04-03 14:52:10 +0000

    banco de dados local

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,9 @@
-//request → Representa a requisição feita pelo cliente (ex: navegador, Postman).
-//response → Permite enviar uma resposta ao cliente.
+import database from "../../../../infra/database.js";
 
-function status(request, response) {
-  response.status(200).json({
-    chave: "São acima da média",
-  });
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum");
+  console.log(result.rows);
+  response.status(200).json({ chave: "são acima da média" });
 }
 
 export default status;
